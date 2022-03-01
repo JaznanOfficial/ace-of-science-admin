ea27402f009cb658d1f23f3181954f4191d51a05
JaznanOfficial <jaznanofficial@gmail.com>
2022-03-01 13:06:31 +0600

    some changes

--- src/components/Magazine/Magazine.jsx
+++ src/components/Magazine/Magazine.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Card, Col, Row } from "react-bootstrap";
 
 const Magazine = () => {
     return (
@@ -14,7 +15,6 @@ const Magazine = () => {
                         aria-describedby="emailHelp"
                         placeholder="Pdf Link"
                     />
-                    
                 </div>
                 <div class="form-group mt-3">
                     <label for="exampleInputPassword1">Image that will show to user</label>
@@ -25,11 +25,36 @@ const Magazine = () => {
                         placeholder="Image Link"
                     />
                 </div>
-                
+
                 <button type="submit" class="btn btn-primary mt-3">
                     Submit
                 </button>
             </form>
+
+            <div className="container my-5">
+                <h1 className="text-danger">Posted All Magazines</h1>
+
+                <Row xs={1} md={3} className="g-4">
+                    {Array.from({ length: 4 }).map((_, idx) => (
+                        <Col>
+                            <Card className="shadow">
+                                <Card.Img
+                                    variant="top"
+                                    src="https://thumbs.dreamstime.com/b/freedom-concept-silhouettes-broken-chain-birds-flying-sky-180470108.jpg"
+                                    className="m-3"
+                                />
+                                
+                                <Card.Footer>
+                                    
+                                    <Button variant="outline-danger" className="m-1">
+                                        <strong>Remove this Magazine</strong>
+                                    </Button>
+                                </Card.Footer>
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+            </div>
         </div>
     );
 };
