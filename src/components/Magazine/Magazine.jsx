4a12185054f33b043430fc59c0712a7c04959f66
JaznanOfficial <jaznanofficial@gmail.com>
2022-03-01 10:12:52 +0600

    magazing done

--- src/components/Magazine/Magazine.jsx
+++ src/components/Magazine/Magazine.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React from "react";
 
 const Magazine = () => {
     return (
-        <div>
-            <h1>hello world</h1>
+        <div className="container">
+            <h1>Magazine</h1>
+            <form>
+                <div class="form-group">
+                    <label for="pdfLink">Link of pdf</label>
+                    <input
+                        type="text"
+                        class="form-control"
+                        id="exampleInputEmail1"
+                        aria-describedby="emailHelp"
+                        placeholder="Pdf Link"
+                    />
+                    
+                </div>
+                <div class="form-group mt-3">
+                    <label for="exampleInputPassword1">Image that will show to user</label>
+                    <input
+                        type="text"
+                        class="form-control"
+                        id="exampleInputPassword1"
+                        placeholder="Image Link"
+                    />
+                </div>
+                
+                <button type="submit" class="btn btn-primary mt-3">
+                    Submit
+                </button>
+            </form>
         </div>
     );
 };
