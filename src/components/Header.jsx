cce0b31a18e564a72b82f199a54daea2a746d3f1
JaznanOfficial <jaznanofficial@gmail.com>
2022-03-01 09:06:51 +0600

    some changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,54 +1,62 @@
-import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import { useStateValue } from '../store/Store';
+import React from "react";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useStateValue } from "../store/Store";
 
 export default function Header() {
-  const [{ user }] = useStateValue();
+    const [{ user }] = useStateValue();
 
-  const logOut = () => {
-    // eslint-disable-next-line no-restricted-globals
-    if (confirm('Are you sure to logout?')) {
-      localStorage.clear();
-      // eslint-disable-next-line no-restricted-globals
-      location.reload();
-    }
-  };
+    const logOut = () => {
+        // eslint-disable-next-line no-restricted-globals
+        if (confirm("Are you sure to logout?")) {
+            localStorage.clear();
+            // eslint-disable-next-line no-restricted-globals
+            location.reload();
+        }
+    };
 
-  return (
-    <>
-      <Navbar bg="primary" variant="dark">
-        <Container>
-          <Link className="navbar-brand" to="/">
-            AOS
-          </Link>
+    return (
+        <>
+            
 
-          <Nav className="me-auto">
-            <Link className="nav-link" to="adminusers">
-              AdminUsers
-            </Link>
-            <Link className="nav-link" to="blog">
-              Blog
-            </Link>
-            <Link className="nav-link" to="lesson">
-              Lesson
-            </Link>
-            <Link className="nav-link" to="review">
-              review
-            </Link>
-          </Nav>
-          <span className="text-white">{user.name}</span>
+            <Navbar bg="primary" variant="dark" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">Ace Of Science</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            
+                                <Link className="nav-link" to="adminusers">
+                                    Admin-Users
+                                </Link>
+                                <Link className="nav-link" to="blog">
+                                    Blog
+                                </Link>
+                                <Link className="nav-link" to="lesson">
+                                    Lesson
+                                </Link>
+                                <Link className="nav-link" to="review">
+                                    review
+                                </Link>
+                            
 
-          {user._id && (
-            <button
-              className="btn btn-sm  btn-outline-danger mx-2"
-              onClick={logOut}
-            >
-              LogOut
-            </button>
-          )}
-        </Container>
-      </Navbar>
-    </>
-  );
+                            {user._id && (
+                                
+                                    <span className="text-white mt-1 ms-5">
+                                        {user.name}
+                                        <button
+                                            className="btn btn-sm  btn-outline-danger mx-2"
+                                            onClick={logOut}
+                                        >
+                                            LogOut
+                                        </button>
+                                    </span>
+                                
+                            )}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
 }
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,58 +1,48 @@
 import React from "react";
 import "./Review.css";
 import ReactStars from "react-rating-stars-component";
+import { Col, Row } from "react-bootstrap";
 
 const Review = () => {
     const ratingChanged = (newRating) => {
         console.log(newRating);
     };
     return (
-        <div className="review">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8">
-                        <div class="people-nearby">
-                            <div class="nearby-user">
-                                <div class="row">
-                                    <div class="col-md-2 col-sm-2">
-                                        <img
-                                            src="https://bootdey.com/img/Content/avatar/avatar7.png"
-                                            alt="user"
-                                            class="profile-photo-lg"
-                                        />
-                                    </div>
-                                    <div class="col-md-7 col-sm-7">
-                                        <h5>
-                                            <a href="#" class="profile-link">
-                                                Sophia Page
-                                            </a>
-                                        </h5>
-                                        <p>Software Engineer</p>
-                                        <p class="text-muted">All review text here</p>
-                                        <ReactStars
-                                            count={5}
-                                            onChange={ratingChanged}
-                                            size={24}
-                                            activeColor="#ffd700"
-                                        />
-                                    </div>
-                                    <div class="col-md-3 col-sm-3">
-                                        <button class="btn btn-primary pull-right">
-                                            Add To Homepage
-                                        </button>
-                                        <button class="btn btn-success pull-right mt-3">
-                                            Add to Review page
-                                        </button>
-                                        <button class="btn btn-danger pull-right mt-3">
-                                            Remove
-                                        </button>
-                                    </div>
-                                </div>
+        <div class="container my-3">
+            <Row xs={1} md={1} className="g-4">
+                <Col className="border py-3">
+                    <div class="d-lg-flex justify-content-around align-items-center text-center">
+                        <div>
+                            <img
+                                src="https://bootdey.com/img/Content/avatar/avatar7.png"
+                                alt="user"
+                                class="profile-photo-lg"
+                            />
+                            <p>Software Engineer</p>
+                        </div>
+                        <div className="w-100 p-5">
+                            
+                            <p class="text-muted text-start">ভাংতি টাকা পকেটে রাখা বা টাকা ভাংতি করার মতো এক্সট্রা ঝামেলা আর হয় না! একে তো কোথাও বের হওয়ার সময় পকেট চেক করে দেখতে হয় ভাংতি টাকা আছে কিনা, তার উপর টাকা ভাঙ্গাতে গেলেও অনেকরকম সমস্যা। এটিএম থেকে তো সবসময় ৫০০ টাকা, ১০০০ টাকার নোটেই দেয়, এগুলো ছোটোখাটো কিছু কিনেও বেশীরভাগ জায়গা থেকে ভাঙ্গানো যায় না। তার উপর কেউ যদি ভাংতি দেয়ও, তখন আমার চেক করতে হয় নোটগুলো ঠিক আছে কিনা। একটা সময় দুই টাকার নোট বেশীরভাগই পুরনো, ছিঁড়া-ফাড়া পেতাম, কিন্তু এখন সেই লিস্টে ৫ টাকা, ১০ টাকা, ২০ টাকা, ৫০ টাকা এমনকি ১০০ টাকার নোটও চলে আসছে। এই ছিঁড়া-ফাড়া নোট নিয়া আবার আরেক ঝামেলা। তাই বাধ্য হয়ে এখন যেখানে ডিজিটাল পেমেন্ট পাই, সেখান থেকেই যা কেনার কিনি। এখন বেশীরভাগ ডিজিটাল পেমেন্ট অ্যাক্সপ্ট করে এমন জায়গায় জিনিসপত্রের দামও হালকা বেশী হয়। বাট টাকা ভাংতি করা বা আরো বাড়তি ঝামেলার চাইতে ডিজিটালি টাকা ট্রানজেকশন করাই বেটার... </p>
+                            <div className="text-center">
+                            <ReactStars
+                                count={5}
+                                onChange={ratingChanged}
+                                size={24}
+                                activeColor="#ffd700"
+                                
+                            />
+                            </div>
+                            <div className="d-lg-flex flex-row justify-content-center align-items-center mt-3">
+                                <button class="btn btn-primary">Add To Homepage</button>
+                                <button class="btn btn-success">
+                                    Add to Review page
+                                </button>
+                                <button class="btn btn-danger">Remove</button>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                </Col>
+            </Row>
         </div>
     );
 };
