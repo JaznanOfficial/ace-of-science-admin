8df92ba0e3d1400aec5bc931183da7e8313aac89
JaznanOfficial <jaznanofficial@gmail.com>
2022-03-28 20:21:10 +0600

    blog data post

--- src/page/Blog.jsx
+++ src/page/Blog.jsx
@@ -1,31 +1,62 @@
-import React, { useState } from "react";
-import { Button, Card, Col, Container, Row } from "react-bootstrap";
-
+import React, { useRef } from "react";
+import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
 import "./Blog.css";
-import { EditorState } from "draft-js";
-import { Editor } from "react-draft-wysiwyg";
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-export default function Blog() {
-    const [editorState, seteditorState] = useState(EditorState.createEmpty());
 
-    const onEditorStateChange = (editorState) => {
-        seteditorState(editorState);
-    };
+const Blog = () => {
+    const imageRef = useRef();
+    const headingRef = useRef();
+    const textRef = useRef();
+
+
+
+
+    const blogHandler = (e) => {
+        e.preventDefault();
+        const imageLink = imageRef.current.value;
+        const heading = headingRef.current.value;
+        const text = textRef.current.value;
+        const blogs = { imageLink, heading, text };
+        console.log(blogs);
+        
+        fetch("https://enigmatic-crag-58614.herokuapp.com/blogs", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify(blogs),
+        });
+    }
+
     return (
-        <>
+        <div>
             <Container className="my-3">
                 <h1 className="text-danger text-center">Write here your blog</h1>
-                <Editor
-                    editorState={editorState}
-                    className="border"
-                    toolbarClassName="toolbarClassName"
-                    wrapperClassName="wrapperClassName"
-                    editorClassName="editorClassName"
-                    onEditorStateChange={onEditorStateChange}
-                />
-                <Button variant="outline-danger" className="m-1">
-                    <strong>Post this Blog</strong>
-                </Button>
+
+                <Form onSubmit={blogHandler}>
+                    <Form.Group className="mb-3" controlId="formBasicText">
+                        <Form.Label>Blog Image</Form.Label>
+                        <Form.Control type="text" ref={imageRef} placeholder="Enter Blog Image Link" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicText">
+                        <Form.Label>Blog Heading</Form.Label>
+                        <Form.Control type="text" ref={headingRef} placeholder="Enter Blog Heading" />
+                    </Form.Group>
+                    <div class="form-group">
+                        <label for="exampleFormControlTextarea1">Blog Body</label>
+                        <textarea
+                            class="form-control my-3"
+                            id="exampleFormControlTextarea1"
+                            rows="7"
+                            placeholder="Write Blog Text Here"
+                            ref={textRef}
+                        ></textarea>
+                    </div>
+
+                    <Button variant="outline-danger" type="submit" className="m-1">
+                        <strong>Post this Blog</strong>
+                    </Button>
+                </Form>
             </Container>
             <div className="container my-5">
                 <h1 className="text-danger">Posted All Blogs</h1>
@@ -52,16 +83,18 @@ export default function Blog() {
                                         <a href="#" className="see-more">
                                             See More <i class="fas fa-arrow-circle-right"></i>{" "}
                                         </a>
-                            </strong>
-                            <Button variant="outline-danger" className="m-1">
-                    <strong>Remove this Blog</strong>
-                </Button>
+                                    </strong>
+                                    <Button variant="outline-danger" className="m-1">
+                                        <strong>Remove this Blog</strong>
+                                    </Button>
                                 </Card.Footer>
                             </Card>
                         </Col>
                     ))}
                 </Row>
             </div>
-        </>
+        </div>
     );
-}
+};
+
+export default Blog;
